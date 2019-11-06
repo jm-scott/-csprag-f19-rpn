bf4cdb37c6897736b2bc9951bee414cb470cb8ba
James Scott <jmscot@umich.edu>
2019-11-06 13:47:31 -0500

    Initial commit

---
+++ rpn.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import operator
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.floordiv,
+}
+
+
+def calculate(string: str) -> int:
+    stack = list()
+    for token in string.split():
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            try:
+                function = operator.__dict__[token]
+            except KeyError:
+                function = operators[token]
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                result = function(arg1, arg2)
+                stack.append(result)
+
+    print(stack)
+    if len(stack) != 1:
+        raise TypeError('Malformed input: ' + string)
+    return stack.pop()
+
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+
+if __name__ == '__main__':
+    main()
